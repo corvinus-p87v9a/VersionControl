2a03e3add3fba82badb51fed7460a0dc593bf3a5
Miklós Schieszler <miklos.schieszler@stud.uni-corvinus.hu>
2021-10-01 13:49:15 +0200

    6. rész

--- 2.het/2.het/Form1.cs
+++ 2.het/2.het/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _2.het.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,12 +13,29 @@ namespace _2.het
 {
     public partial class Form1 : Form
     {
+        BindingList<user> users = new BindingList<user>();
+
         public Form1()
         {
             InitializeComponent();
             label1.Text = Resource1.LastName; // label1
             label2.Text = Resource1.FirstName; // label2
             button1.Text = Resource1.Add; // button1
+
+            // listbox1
+            listBox1.DataSource = users;
+            listBox1.ValueMember = "ID";
+            listBox1.DisplayMember = "FullName";
+        }
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            var u = new user()
+            {
+                LastName = textBox1.Text,
+                FirstName = textBox2.Text
+            };
+            users.Add(u);
         }
     }
 }
